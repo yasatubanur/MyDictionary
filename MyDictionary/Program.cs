a22d29adc7657b3cfaea7977f4d69d6fb78a747d
TUBA NUR YAŞA <l5216052@std.yildiz.edu.tr>
2021-02-20 20:44:00 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictonary<int, string> pazarListesi = new Dictonary<int, string>();
+
+            pazarListesi.Add(1, "Elma");
+            pazarListesi.Add(2, "Muz");
+            pazarListesi.Add(3, "Kivi");
+            pazarListesi.Add(4, "Şeftali");
+            pazarListesi.Add(5, "Karpuz");
+            pazarListesi.Add(6, "Erik");
+
+            Console.WriteLine(pazarListesi.Count);
+        }
+    }
+}
